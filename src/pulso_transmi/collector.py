72d6f4a2ee3220b7853ef86fbb351684e5030f8f
Mari <mariaisabell1203@gmail.com>
2026-09-23 15:08:37 -0500

    Compatibiliza colector con pooler de Supabase

--- src/pulso_transmi/collector.py
+++ src/pulso_transmi/collector.py
@@ -73,7 +73,7 @@ def collect(
         for filename in files:
             client.download(filename, destination / filename)
 
-    with psycopg.connect(database_url) as connection:
+    with psycopg.connect(database_url, prepare_threshold=0) as connection:
         stations = upsert_rows(
             connection,
             "stations",
